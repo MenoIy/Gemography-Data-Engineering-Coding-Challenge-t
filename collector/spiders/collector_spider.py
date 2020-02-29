b69a259ec021b91da95c8dd1e52249cf0c834480
MenoIy <karim.daou.1993@gmail.com>
2020-02-29 23:27:43 +0100

    Scraping (title , link, summary, tag) done ; saving result in a csv file

--- collector/spiders/collector_spider.py
+++ collector/spiders/collector_spider.py
@@ -1,6 +1,16 @@
 import scrapy
+import pandas as pd
 
-URL = "https://www.bbc.com"
+URL = "https://www.bbc.com/"
+
+
+
+
+def clean(string):
+    value = string.encode('utf8')
+    value = value.strip()
+    return value
+
 
 class CollectorSpider(scrapy.Spider):
     name = "collector"
@@ -8,8 +18,35 @@ class CollectorSpider(scrapy.Spider):
     def start_requests(self):
         yield scrapy.Request(url=URL, callback=self.parse)
 
+
     def parse(self, response):
-        filename = 'collected.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        
+        contents_list = []
+        
+        media_contents = response.css('div.media__content')
+        for media_content in media_contents:
+            content = {}
+
+            media_title = media_content.css('h3.media__title')
+            title = media_title.xpath('a//text()').extract()
+            if title :
+                content['title'] = clean(title[0])
+
+            href = media_title.xpath('a//@href').extract()
+            if href :
+                content['href'] = clean(href[0])
+
+            tag = media_content.xpath('a//text()').extract()
+            if tag:
+                content['tag'] = clean(tag[0])
+
+            summary = media_content.xpath('p//text()').extract()
+            if summary:
+                content['summary'] = clean(summary[0])
+             
+            contents_list.append(content)
+        
+        df = pd.DataFrame(contents_list)
+        df.to_csv('./bbc.csv', index=False)
+            
+         
