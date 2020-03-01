ac959d1aa0ecab5ef9d23393d81e68f4a1b43e11
MenoIy <karim.daou.1993@gmail.com>
2020-03-01 22:48:28 +0100

    getting the keyword list ; fixing author list ; need to scrap the article and add some comments

--- collector/spiders/collector_spider.py
+++ collector/spiders/collector_spider.py
@@ -10,24 +10,6 @@ class CollectorSpider(scrapy.Spider):
     allowed_domains = ['theguardian.com']
     start_urls = ['https://www.theguardian.com/']
 
-    def clean(self, string):
-        encoded = string.encode('utf8')
-        cleaned = encoded.strip()
-        return cleaned
-
-    def clean_list(self, list):
-        for x in list:
-            x = x.encode('utf8')
-
-
-    def extract_authors(self, authors_list):
-        authors = []
-        for author in authors_list:
-            cleanded = self.clean(author)
-            name = cleanded.split('/')[-1]
-            authors.append(name)
-        return authors
-
 
     def start_requests(self):
         yield scrapy.Request(url='https://www.theguardian.com/', callback=self.parse)
@@ -37,8 +19,7 @@ class CollectorSpider(scrapy.Spider):
         media_links = response.selector.xpath('//a[@data-link-name="article"] //@href').extract()
 
         for media_link in media_links :
-            link = self.clean(media_link)
-            yield scrapy.Request(url=link, callback=self.collect_content)
+            yield scrapy.Request(url=media_link, callback=self.collect_content)
 
     def collect_content(self, response):
         media_content = response.xpath('//html')
@@ -48,13 +29,15 @@ class CollectorSpider(scrapy.Spider):
         link = media_content.xpath("//meta[@property='og:url']/@content").extract()
         publish_time = media_content.xpath("//meta[@property='article:published_time']/@content").extract()
         description = media_content.xpath("//meta[@property='og:description']/@content").extract()
+        keywords = media_content.xpath("//meta[@name='keywords']/@content").extract()
 
 
         media = CollectorItem()
 
-        media['title'] = self.clean(title)
-        media['author'] = self.extract_authors(authors)
-        media['link'] = self.clean(link[0]) 
-        media['publish_time'] = self.clean(publish_time[0])
-        media['description'] = self.clean(description[0])
+        media['title'] = title
+        media['author'] = [author.split('/')[-1] for author in authors]
+        media['link'] = link[0]
+        media['publish_time'] = publish_time[0]
+        media['description'] = description[0]
+        media['keywords'] = [word for keyword in keywords for word in keyword.split(",")]
         yield media
