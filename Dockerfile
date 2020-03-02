6b3d19ee777f596b31a3ca6f1e0dfab1922a77a7
karim daou <karim.daou.1993@gmail.com>
2020-03-02 14:15:16 +0100

    add a docker file for api

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM ubuntu:16.04
+
+RUN apt-get update -y && \
+    apt-get install -y python-pip python-dev
+
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+COPY ./api /app
+
+ENTRYPOINT [ "python" ]
+
+EXPOSE 8001
+
+CMD [ "app.py" ]
--- api/app.py
+++ api/app.py
@@ -1,13 +1,23 @@
-import  requests
-import pymongo
+# import  requests
+# import pymongo
 
-client = pymongo.MongoClient("mongodb+srv://menoly:sakata-1@cluster0-ehtfy.gcp.mongodb.net/test?retryWrites=true&w=majority")
+# client = pymongo.MongoClient("mongodb+srv://menoly:sakata-1@cluster0-ehtfy.gcp.mongodb.net/test?retryWrites=true&w=majority")
 
-collection = client.theguardian
-db = collection.news
+# collection = client.theguardian
+# db = collection.news
 
 
-news = db.find({'keywords' : 'US news'})
+# news = db.find({'keywords' : 'US news'})
 
-for new in news :
-    print('title : ', new['title'])
+# for new in news :
+#     print('title : ', new['title'])
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hey, we have Flask in a Docker container!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port = 8001)
